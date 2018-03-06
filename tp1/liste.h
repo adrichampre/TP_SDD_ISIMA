3b06799b80f9f5ba2bbdb18df3d2bd1a40b91014
Adrien Champredonde <adrien.champredonde@etu.udamail.fr>
2018-03-06 13:46:22 +0100

    ajout dossier tp2

---
+++ tp1/liste.h
@@ -0,0 +1,11 @@
+#include "tp1.h"
+
+void afficherElt(Liste l);
+Liste insererEnTete(Liste l, int val, int usine);
+Liste nouvelleListe(void);
+void libererListe(Liste l);
+Liste insererEnFin(Liste l, int *periode, int n);
+Liste supprElt(maillon *prec, maillon *suppr);
+void afficherListe(Liste l);
+Liste supprPremierElt(Liste l);
+void libererElt(maillon *m);
